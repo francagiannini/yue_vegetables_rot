62568b3f62d871c2d0548650c2a9a6bfa1a436d5
Franca Giannini-Kurina <au710823@uni.au.dk>
2023-11-13 12:02:39 +0100

    explo

--- exploratory.R
+++ exploratory.R
@@ -7,17 +7,20 @@ theme_update(panel.grid = element_blank())
 
 
 compl<- readxl::read_xlsx(
-    "C:\\Users\\au710823\\OneDrive - Aarhus universitet\\Grøntsagsforsøg\\veg_conc_061123.xlsx",
+    "C:\\Users\\au710823\\OneDrive - Aarhus universitet\\Grøntsagsforsøg\\veg_conc_131123.xlsx",
   sheet = "Sheet2") |> filter(!is.na(`Nitrat  mg/L`)) |> 
-  mutate(aruge=paste(print(`År`),ifelse(Uge>10,print(Uge),print(paste("0",Uge,sep=""))),sep="")) |> 
-  mutate(date=as.Date(aruge,"%Y%W"))
+  tbl_df() %>% 
+  mutate(newar = ymd(paste0(`År`,"-01-01"))) |>
+  #mutate(aruge=paste(print(`År`),ifelse(Uge>10,print(Uge),print(paste("0",Uge,sep=""))),sep="")) |> 
+  mutate(date_week=newar + weeks(Uge))
 
-summary(compl$aruge)
+summary(compl$date_week)
 
 compl |> ggplot(aes(
-  x = date,
+  x = date_week,
   y = `Nitrat  mg/L`,
   col = Sted #interaction(Sted, Område)
 )) +
   geom_point() +
-  geom_smooth()
+  geom_smooth()+
+  theme(axis.text.x = element_text(angle = 90))
